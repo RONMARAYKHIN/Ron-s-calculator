81d366ce3e519e173caed1a4b04fc273e9770992
RONMARAYKHIN <113259827+RONMARAYKHIN@users.noreply.github.com>
2022-10-22 13:49:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function dis(val) {
     document.getElementById("result").value += val
 }
 function opreate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
+    let p = document.getElementById("result").value;
+    let q = eval(p);
     document.getElementById("result").value = q;
 }
 function clr() {
